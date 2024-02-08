09e1c2d50cb30301fbe834d9b0e231904add4238
jarrodschilling <jarrodschilling@gmail.com>
2024-02-08 15:29:10 -0700

    formating form

--- client/src/views/TradeForm.jsx
+++ client/src/views/TradeForm.jsx
@@ -141,6 +141,9 @@ const TradeForm = (props) => {
                 ...prevState,
                 [e.target.name]: newValue
             }))
+            const currentDate = new Date();
+            console.log(currentDate)
+            console.log(stockState.date)
             let errorMsg = ""
             if (!stockState.date) {
                 setFormErrors({...formErrors, date: errorMsg})
@@ -161,6 +164,15 @@ const TradeForm = (props) => {
                 console.log(err);
                 setErrors(err.response.data.errors)
             })
+        setStockState({
+            ticker: "",
+            price: 0,
+            date: 0,
+            shares: 0,
+            buySell: "",
+            shaper: "",
+            tactical: ""
+        })
     }
 
     const validateForm = () => {
@@ -172,72 +184,76 @@ const TradeForm = (props) => {
         <div className="tradeFormContainer">
             <h1>Add a Trade</h1>
             <form className="tradeForm" onSubmit={handleSubmit}>
-                <label htmlFor="ticker">Ticker</label>
-                <input type="text" name="ticker" id="ticker" value={stockState.ticker} onChange={handleChange} />
-                {formErrors.ticker && <p>{formErrors.ticker}</p>}
-                {errors.ticker && <p>{errors.ticker.message}</p>}
+                <div className="formCol">
+                    <label htmlFor="ticker">Ticker</label>
+                    <input type="text" name="ticker" id="ticker" value={stockState.ticker} onChange={handleChange} />
+                    {formErrors.ticker? <p>{formErrors.ticker}</p>: <p>-</p>}
+                    {errors.ticker && <p>{errors.ticker.message}</p>}
 
-                <label htmlFor="price">Price</label>
-                <input type="number" name="price" id="price" value={stockState.price} onChange={handleChange} />
-                {formErrors.price && <p>{formErrors.price}</p>}
-                {errors.price && <p>{errors.price.message}</p>}
+                    <label htmlFor="price">Price</label>
+                    <input type="number" name="price" id="price" value={stockState.price} onChange={handleChange} />
+                    {formErrors.price? <p>{formErrors.price}</p>: <p>-</p>}
+                    {errors.price && <p>{errors.price.message}</p>}
 
-                <label htmlFor="shares">Shares</label>
-                <input type="number" name="shares" id="shares" value={stockState.shares} onChange={handleChange} />
-                {formErrors.shares && <p>{formErrors.shares}</p>}
-                {errors.shares && <p>{errors.shares.message}</p>}
+                    <label htmlFor="shares">Shares</label>
+                    <input type="number" name="shares" id="shares" value={stockState.shares} onChange={handleChange} />
+                    {formErrors.shares? <p>{formErrors.shares}</p>: <p>-</p>}
+                    {errors.shares && <p>{errors.shares.message}</p>}
+                </div>
 
-                <label htmlFor="date">Date</label>
-                <input type="date" name="date" id="date" value={stockState.date} onChange={handleChange} />
-                {formErrors.date && <p>{formErrors.date}</p>}
-                {errors.date && <p>{errors.date.message}</p>}
+                <div className="formCol">
+                    <label htmlFor="date">Date</label>
+                    <input type="date" name="date" id="date" value={stockState.date} onChange={handleChange} />
+                    {formErrors.date? <p>{formErrors.date}</p>: <p>-</p>}
+                    {errors.date && <p>{errors.date.message}</p>}
 
-                <label htmlFor="buySell">Buy or Sell</label>
-                <select name="buySell" id="buySell" value={stockState.buySell} onChange={handleChange}>
-                    <option value="">Pick One</option>
-                    <option value="buy">Buy</option>
-                    <option value="sell">Sell</option>
-                </select>
-                {formErrors.buySell && <p>{formErrors.buySell}</p>}
-                {errors.buySell && <p>{errors.buySell.message}</p>}
+                    <label htmlFor="buySell">Buy or Sell</label>
+                    <select name="buySell" id="buySell" value={stockState.buySell} onChange={handleChange}>
+                        <option value="">Pick One</option>
+                        <option value="buy">Buy</option>
+                        <option value="sell">Sell</option>
+                    </select>
+                    {formErrors.buySell? <p>{formErrors.buySell}</p>: <p>-</p>}
+                    {errors.buySell && <p>{errors.buySell.message}</p>}
+                </div>
 
-                <label htmlFor="shaper">Shaper Pattern</label>
-                <select name="shaper" id="shaper" value={stockState.shaper} onChange={handleChange}>
-                    <option value="none">Pick One</option>
-                    <option value="Cup w/ Handle">Cup w/ Handle</option>
-                    <option value="Cup no Handle">Cup no Handle</option>
-                    <option value="Coil">Coil</option>
-                    <option value="Flat Base">Flat Base</option>
-                    <option value="High Tight Flag">High Tight Flag</option>
-                    <option value="Double Bottom">Double Bottom</option>
-                    <option value="Inverse Head and Shoulders">Inverse Head and Shoulders</option>
-                    <option value="De-risk">De-risk</option>
-                    <option value="Earnings Soon">Earnings Soon</option>
-                </select>
+                <div className="formCol">
+                    <label htmlFor="shaper">Shaper Pattern</label>
+                    <select name="shaper" id="shaper" value={stockState.shaper} onChange={handleChange}>
+                        <option value="none">Pick One</option>
+                        <option value="Cup w/ Handle">Cup w/ Handle</option>
+                        <option value="Cup no Handle">Cup no Handle</option>
+                        <option value="Coil">Coil</option>
+                        <option value="Flat Base">Flat Base</option>
+                        <option value="High Tight Flag">High Tight Flag</option>
+                        <option value="Double Bottom">Double Bottom</option>
+                        <option value="Inverse Head and Shoulders">Inverse Head and Shoulders</option>
+                        <option value="De-risk">De-risk</option>
+                        <option value="Earnings Soon">Earnings Soon</option>
+                    </select>
+                    {/* <input type="text" name="shaper" id="shaper" value={stockState.shaper} onChange={handleChange} /> */}
+                    {notRequired.shaper? <p>{notRequired.shaper}</p>: <p>-</p>}
+                    {errors.shaper && <p>{errors.shaper.message}</p>}
 
-                {/* <input type="text" name="shaper" id="shaper" value={stockState.shaper} onChange={handleChange} /> */}
-                {notRequired.shaper && <p>{notRequired.shaper}</p>}
-                {errors.shaper && <p>{errors.shaper.message}</p>}
-
-                <label htmlFor="tactical">Tactical Pattern</label>
-                <select name="tactical" id="tactical" value={stockState.tactical} onChange={handleChange}>
-                    <option value="none">Pick One</option>
-                    <option value="Mini Coil">Mini Coil</option>
-                    <option value="Kicker">Kicker</option>
-                    <option value="Downtrend Line">Downtrend Line</option>
-                    <option value="Breakout PB to 20EMA">Breakout PB to 20EMA</option>
-                    <option value="Gap Up PB to 8EMA">Gap Up PB to 8EMA</option>
-                    <option value="Pull Back to 50SMA">Pull Back to 50SMA</option>
-                    <option value="First Touch of the 10WK SMA">First Touch of the 10WK SMA</option>
-                    <option value="Kicker">Stop Hit</option>
-                    <option value="De-risking">De-risking</option>
-
-                </select>
-                {/* <input type="text" name="tactical" id="tactical" value={stockState.tactical} onChange={handleChange} /> */}
-                {notRequired.tactical && <p>{notRequired.tactical}</p>}
-                {errors.tactical && <p>{errors.tactical.message}</p>}
-
-                <button className="confirmTrade" type="submit" disabled={!validateForm()}>Confirm Trade</button>
+                    <label htmlFor="tactical">Tactical Pattern</label>
+                    <select name="tactical" id="tactical" value={stockState.tactical} onChange={handleChange}>
+                        <option value="none">Pick One</option>
+                        <option value="Mini Coil">Mini Coil</option>
+                        <option value="Kicker">Kicker</option>
+                        <option value="Downtrend Line">Downtrend Line</option>
+                        <option value="Breakout PB to 20EMA">Breakout PB to 20EMA</option>
+                        <option value="Gap Up PB to 8EMA">Gap Up PB to 8EMA</option>
+                        <option value="Pull Back to 50SMA">Pull Back to 50SMA</option>
+                        <option value="First Touch of the 10WK SMA">First Touch of the 10WK SMA</option>
+                        <option value="Kicker">Stop Hit</option>
+                        <option value="De-risking">De-risking</option>
+                    </select>
+                    {/* <input type="text" name="tactical" id="tactical" value={stockState.tactical} onChange={handleChange} /> */}
+                    {notRequired.tactical? <p>{notRequired.tactical}</p>: <p>-</p>}
+                    {errors.tactical && <p>{errors.tactical.message}</p>}
+                
+                    <button className="confirmTrade" type="submit" disabled={!validateForm()}>Confirm Trade</button>
+                </div>
             </form>
         </div>
     )
